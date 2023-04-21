f645497d7aee7281df2cefd89e9b368b250375f7
david <david.herve@ynov.com>
2023-04-21 09:33:16 +0200

    first commit

---
+++ src/test/java/com/barkeeper/barkeeperserver/BarkeeperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barkeeper.barkeeperserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkeeperServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
